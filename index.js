ba2849894fca977b7f976ae320c46c2f22392dc9
tomashahas <133536265+tomashahas@users.noreply.github.com>
2023-06-10 12:15:20 +0000

    cors and express.json

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // index.js
 import express from "express";
+import cors from "cors";
 
 const app = express()
 const PORT = 4000
+
+app.use(express.json());
+app.use(cors());
+
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
