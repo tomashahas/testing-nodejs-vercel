632f47f6dec00f5c2176466e199553709aeac76a
tomashahas <133536265+tomashahas@users.noreply.github.com>
2023-06-10 12:18:09 +0000

    doenv

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // index.js
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
-const app = express()
-const PORT = 4000
 
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
+
+
 
 
 app.listen(PORT, () => {
